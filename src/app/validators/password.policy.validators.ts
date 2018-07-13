50ab169e944a50ef9997c33134fb39b36a4c0d63
Witaya Tospitakkul <witaya1412@gmail.com>
2018-07-13 21:37:37 +0700

    remove some policy in password

--- src/app/validators/password.policy.validators.ts
+++ src/app/validators/password.policy.validators.ts
@@ -3,14 +3,12 @@ import { AbstractControl, ValidationErrors } from '@angular/forms';
 export class PasswordValidator {
 
     static validate(control: AbstractControl): ValidationErrors | null {
-        let hasNumber = /\d/.test(control.value);
         let hasUpper = /[A-Z]/.test(control.value);
         let hasLower = /[a-z]/.test(control.value);
-        const valid = hasNumber && hasUpper && hasLower;
+        const valid = hasUpper && hasLower;
         if (!valid) {
             return {
                 valid,
-                hasNumber,
                 hasUpper,
                 hasLower
             };
