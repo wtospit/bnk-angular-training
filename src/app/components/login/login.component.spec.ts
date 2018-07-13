63304dfad68783552efc3562a440a9b2b2e0dd9a
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-07-13 16:03:49 +0700

    Fix test

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from '../../../../node_modules/@angular/forms';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,9 +9,12 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [LoginComponent],
+      imports: [
+        ReactiveFormsModule
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
